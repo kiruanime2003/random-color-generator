623e226f989113b2016ad107b89702b2610e4579
kiruanime2003 <kiruanime2003@gmail.com>
2022-12-08 17:35:28 +0530

    completed partial stack

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+
     var colorDisplayBox = document.getElementById("color-box");
     var hexValue = document.querySelector("span");
     var svgIcon = document.querySelector("svg");
     var valueArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
     var randomHexValue = ["#"];
+    var undoHexValue = ["#06D7D7"];
 
     window.addEventListener("keydown", changeBackgroundColor);
 
@@ -21,12 +23,20 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     function changeBackgroundColor(event){
         if(event.key === " "){
+            undoHexValue.push(calcHexValue());
             colorDisplayBox.style.backgroundColor = calcHexValue();
             hexValue.innerHTML = calcHexValue();
+            console.log(undoHexValue);
             randomHexValue = ["#"];
         }
-        
     }
+
+    window.addEventListener("keydown", (event)=>{
+        if(event.ctrlKey && event.key == "z"){
+            undoHexValue.pop();
+            console.log(undoHexValue.pop());
+        }
+    });
 
     svgIcon.addEventListener("click", function(){
         navigator.clipboard.writeText(hexValue.innerHTML);
